685ebaf3cce64f117a06924d4af9ca866a6c85b0
Arman <arman_096@bk.ru>
2026-03-08 23:49:21 +0500

    Add Yandex metrics

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import MainLayout from "@/components/layout/MainLayout";
 import { roboto } from "@/app/fonts";
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages, getTranslations } from 'next-intl/server';
+import Script from 'next/script';
 
 export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }): Promise<Metadata> {
   const { locale } = await params;
@@ -41,6 +42,27 @@ export default async function RootLayout(props: {
     <html lang={locale} className={`${roboto.variable}`}>
       <body className={`antialiased font-roboto`}>
         <NextIntlClientProvider messages={messages}>
+          <Script id="yandex-metrika" strategy="lazyOnload">
+            {`
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(96119899, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+              });
+            `}
+          </Script>
+          <noscript>
+            <div>
+              <img src="https://mc.yandex.ru/watch/96119899" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+            </div>
+          </noscript>
           <MainLayout>{children}</MainLayout>
         </NextIntlClientProvider>
       </body>
